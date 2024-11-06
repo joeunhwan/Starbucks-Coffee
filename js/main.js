8cbaa3cfbfd09aa5f6a7e8e61a14304f18b52dec
joeunhwan <eunhwan119@gmail.com>
2024-11-06 15:42:03 +0900

    브랜치 로그인 페이지

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click',function(){
-    searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 //배지 스크롤 페이드
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -154,8 +138,3 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-//푸터 카피라이트 년도 설정
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
